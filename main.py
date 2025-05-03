5c805b79693c0a1b3f0fa88e530d90d09e7c5be6
Zeldork93 <katerinadf1@gmail.com>
2025-05-02 20:31:18 -0700

    Sorted character list

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from stats import get_word_count
 
 from stats import get_character_count
 
+from stats import sort_on
+
+from stats import make_count_list
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         text = f.read()
@@ -9,6 +13,12 @@ def get_book_text(path_to_file):
 
 def main():
     file_contents = get_book_text("books/frankenstein.txt")
-    print(get_character_count(file_contents))
+    char_dict = get_character_count(file_contents)
+    count_list = make_count_list(char_dict)
+    count_list.sort(reverse=True, key=sort_on)
+    print(f"Found {get_word_count(file_contents)} total words")
+    for i in range(len(count_list)):
+        print(f"{count_list[i]["char"]}: {count_list[i]["num"]}")
+    
 
 main()
--- stats.py
+++ stats.py
@@ -5,8 +5,18 @@ def get_word_count(word_count):
 def get_character_count(text):
     character_count = {}
     for i in text:
-        if i.lower() in character_count:
-            character_count[i.lower()] += 1
-        else:
-            character_count[i.lower()] = 1
+        if i.isalpha():
+            if i.lower() in character_count:
+                character_count[i.lower()] += 1
+            else:
+                character_count[i.lower()] = 1
     return character_count
+
+def sort_on(dict):
+    return dict["num"]
+
+def make_count_list(dict):
+    count_list = []
+    for i in dict:
+        count_list.append({"char": i, "num": dict[i]})
+    return count_list
