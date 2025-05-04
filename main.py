5037d24f9ec1df9fc96a741104157937d25f6e2d
Zeldork93 <katerinadf1@gmail.com>
2025-05-04 11:54:57 -0700

    Add more books

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from stats import sort_on
 
 from stats import make_count_list
 
+import sys
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         text = f.read()
     return text
 
-def main():
-    file_contents = get_book_text("books/frankenstein.txt")
+def main(path_to_book):
+    file_contents = get_book_text(path_to_book)
     char_dict = get_character_count(file_contents)
     count_list = make_count_list(char_dict)
     count_list.sort(reverse=True, key=sort_on)
@@ -20,5 +22,8 @@ def main():
     for i in range(len(count_list)):
         print(f"{count_list[i]["char"]}: {count_list[i]["num"]}")
     
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
-main()
+main(sys.argv[1])
